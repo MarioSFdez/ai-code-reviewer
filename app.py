5fcb53df5ef17e270e99c5b8fdb12813031c04ad
MarioSFdez <mariosfdez@gmail.com>
2025-11-15 17:28:11 +0100

    add: authentication github

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from github import Auth, Github
 pat_token = os.getenv("PAT_TOKEN")
 
 if not pat_token:
-    raise RuntimeError("Falta token de autenticación en el entorno") 
+    raise RuntimeError("Falta el token de autenticación en el entorno") 
 
 # Autenticación en Github
 auth = Auth.Token(pat_token)
